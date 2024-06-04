024ebfd3f2e1e781c995438506b9e31b25312c6a
ELEFTHERIOSAMITSIS <lefterisamitsis@gmail.com>
2024-06-04 14:26:06 +0300

    DONE

--- clustering.py
+++ clustering.py
@@ -1,32 +1,7 @@
-from sklearn.cluster import MiniBatchKMeans,KMeans,DBSCAN,Birch
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.cluster import MiniBatchKMeans,DBSCAN,Birch
 from sklearn.metrics import silhouette_score
-from sklearn.metrics import davies_bouldin_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
 import pandas as pd
-import numpy as np
 
-def kmeans(data,labels):
-    x=data
-    y=labels
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
-    X_train_std = StandardScaler().fit_transform(X_train)
-
-    inertias = []
-    silhouette_scores = []
-    db_scores = []
-    K_range = range(1, 10)  # Adjust the range based on your dataset
-
-    for k in K_range:
-        kmeans = KMeans(n_clusters=k, random_state=11, n_init='auto').fit(X_train_std)
-        inertias.append(kmeans.inertia_)
-        if(k != 1):
-            silhouette_scores.append(silhouette_score(X_train_std, kmeans.labels_))
-            db_scores.append(davies_bouldin_score(X_train_std, kmeans.labels_))
-    print(silhouette_scores)
-     
 
 
 def minibatch_kmeans(data,labels,clusters):
@@ -38,18 +13,20 @@ def minibatch_kmeans(data,labels,clusters):
 
     silhouette_score_val=silhouette_score(data, mbk.labels_)
     print(silhouette_score_val)
- 
+    
+
     df = pd.DataFrame({'real_labels': labels, 'clusters': mbk.labels_})
 
     return mbk.labels_,df,silhouette_score_val
 
-def dbscan(data,labels):
+def dbscan(data,labels,eps1):
    
-    dbs = DBSCAN(n_jobs=-1,min_samples=50,eps=1)
+    dbs = DBSCAN(n_jobs=-1,min_samples=50,eps=eps1)
     dbs.fit(data)
 
     silhouette_score_val=silhouette_score(data, dbs.labels_)
     print(f'DBSCAN silouette score --->{silhouette_score_val}')
+
 
     df = pd.DataFrame({'real_labels': labels, 'clusters': dbs.labels_})
 
@@ -66,3 +43,13 @@ def birch(data,labels,clusters,th,bf):
     df = pd.DataFrame({'real_labels': labels, 'clusters': birch.labels_})
 
     return birch.labels_,df,silhouette_score_val
+
+def purity_metric(df):
+    dominant_class_counts = df.groupby(level='clusters').max().sum()
+
+    total_instances = df.sum()
+
+    purity = dominant_class_counts / total_instances
+
+    print(f"Purity: {purity}")
+
